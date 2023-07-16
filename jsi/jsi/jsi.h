9705d712b60b8603b219df9e345e40e5f3530f8f
Vladimir Morozov <vmorozov@microsoft.com>
2023-07-16 09:24:19 -0700

    Use cdecl for PointerValue

--- jsi/jsi/jsi.h
+++ jsi/jsi/jsi.h
@@ -309,7 +309,7 @@ class JSI_EXPORT Runtime {
   // rvalue arguments/methods would also reduce the number of clones.
 
   struct PointerValue {
-    virtual void __stdcall invalidate() = 0;
+    virtual void __cdecl invalidate() = 0;
 
    protected:
     virtual ~PointerValue() = default;
